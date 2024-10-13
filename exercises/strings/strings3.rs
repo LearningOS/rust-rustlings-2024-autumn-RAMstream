5ef2b1e1ed1792d133dc4c12afaf75b7bfe0011e
wangjunze <2321596346@qq.com>
2024-10-13 09:58:19 +0800

    solve strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,23 +3,38 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let res=input.trim();
+    res.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let s=input.to_string();
+    s+" world!"
+
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s=input.to_string();
+    let mut res=String::new();
+    for i in s.split_whitespace()
+    {
+        let word=i.to_string();
+        if word=="cars"
+        {
+            res=res+"balloons ";
+        }
+        else {
+            res=res+i+" ";
+        }
+    }
+    let ed=res.len();
+    res[0..ed-1].to_string()
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
